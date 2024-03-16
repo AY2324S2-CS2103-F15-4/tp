946b88885aa8a9f0b06f454057cab664aea8d038
DOMINICCOM\fudom <e1024876@u.nus.edu>
2024-03-17 03:05:37 +0800

    Changed the find function to be able to find tags as well as names. However, the tags would only return the 1st entry with the resulting tag, so I need to figure out how to show all of the tags.

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -1,10 +1,13 @@
 package seedu.address.model.person;
 
 import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.tag.Tag;
 
 /**
  * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
@@ -12,14 +15,26 @@ import seedu.address.commons.util.ToStringBuilder;
 public class NameContainsKeywordsPredicate implements Predicate<Person> {
     private final List<String> keywords;
 
+
     public NameContainsKeywordsPredicate(List<String> keywords) {
         this.keywords = keywords;
     }
 
     @Override
     public boolean test(Person person) {
+        Predicate<String> personName = keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword);
+        Predicate<String> personTag = keyword -> StringUtil.containsWordIgnoreCase(person.getTags().stream()
+                .map(tag -> tag.tagName).collect(Collectors.joining()), keyword);
+//        System.out.println(person.getName());
+//        Set<Tag> tags = person.getTags();
+//        for (Tag tag : tags) {
+//            System.out.println(tag.tagName);
+//        }
+//        System.out.println();
+//        System.out.println(person.getTags().stream().map(tag -> tag.tagName));
+        Predicate<String> combinedCondition = personName.or(personTag);
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+                .anyMatch(combinedCondition);
     }
 
     @Override
